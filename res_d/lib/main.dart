371826cd162d26fbc9e98211d8072f08311cb601
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-06-21 15:32:26 +0530

    Update main.dart

--- res_d/lib/main.dart
+++ res_d/lib/main.dart
@@ -4,10 +4,7 @@ import 'package:flutter/material.dart';
 import 'homepage.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(
-  DevicePreview(
-    builder: (context) => MyApp(), // Wrap your app
-  ),
+void main() => runApp(MyApp(),
 );
 
 class MyApp extends StatelessWidget {
